b57f2416b49991b9ab5be741ea96f1bcfa0c40e0
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2022-03-03 09:49:37 +0100

    Update fifafurbo.py

--- fifafurbo.py
+++ fifafurbo.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.title('FIFAFURBO!')
 st.write('Aquí descubrirás todo lo que el FIFA te puede ofrecer...')
-prueba = pd.read_csv("C:\Users\tomas\OneDrive - UPV\PRIMERO\2o cuatrimestre (1b)\PRG\PRÁCTICAS\resultados_elecciones_2019.csv", sep=';')
+prueba = pd.read_csv("resultados_elecciones_2019.csv", sep=';')
 prueba.head()
 
